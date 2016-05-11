b4ffc796aba70897e7e18cf87808ea3eb400bd4c
sudasf <1518223884@qq.com>
2016-05-11 18:15:02 +0800

    change

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session')
+var RedisStore = require('connect-redis')(session);
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,10 +23,53 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  store: new RedisStore(),
+  secret: 'sunfeng love panpan', // 建议使用 128 个字符的随机字符串
+  cookie: { maxAge: 20*60 * 1000 }
+})).use(check)
+
 
 app.use('/', routes);
 app.use('/users', users);
 
+function check(req,res,next){
+  var user=req.session.user
+  res.locals.checkUser=user?1:0
+  res.locals.user=user
+  //console.log(res.locals.checkUser)
+  next()
+}
+
+function setCookie(req,res){
+   if (req.cookies.isVisit) {
+      console.log(req.cookies);
+      console.log(req.signedCookies);
+      res.send("再次欢迎访问");
+    } else {
+      res.cookie('isVisit', 1, {maxAge: 60 * 1000,httpOnly:true});
+      res.send("欢迎第一次访问");
+    }
+}
+function setSession(req,res){
+  // 检查 session 中的 isVisit 字段
+  // 如果存在则增加一次，否则为 session 设置 isVisit 字段，并初始化为 1。
+  if(req.session.isVisit) {
+    req.session.isVisit++;
+    //console.log(req.session)
+    res.send('<p>第 ' + req.session.isVisit + '次来此页面</p>');
+    console.log(req.session);
+
+    
+  } else {
+    req.session.isVisit = 1;
+    req.session.name='孙峰'
+    req.session.password='sf123'
+    res.send("欢迎第一次来这里");
+    console.log(req.session);
+
+  }
+}
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,93 @@
 var express = require('express');
 var router = express.Router();
+var mongoose=require('mongoose')
+var bcrypt=require('bcrypt')
+var db=mongoose.connect('mongodb://localhost/test')
+var UserSchema=new mongoose.Schema({
+	name:String,
+	password:String,
+})
+
+var User=mongoose.model('User',UserSchema)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
+
+router.get('/login',function(req, res, next){
+	res.render('login');
+}).post('/login',function(req,res,next){
+	var obj=req.body
+	//console.log(obj)
+	var username=obj.name
+	var psd=obj.password
+	if(username&&psd){
+		User.findOne({name:username},function(err,doc){
+			if(err) return next(err)
+			//console.log(setHash(psd))
+			if(doc){
+				if(doc.password==psd){
+				req.session.user=username
+				res.redirect('/home')
+				}else{
+					res.redirect('/login')
+				}
+			}else{
+				res.redirect('/login')
+			}
+			
+		})
+	}else{
+		res.redirect('/login')
+	}
+})
+
+router.get('/register',function(req, res, next){
+
+	res.render('register');
+}).post('/register',function(req, res, next){
+	var obj=req.body
+	//console.log(obj)
+	var username=obj.name
+	var psd=obj.password
+	if(username&&psd){
+		User.find({name:username},function(err,doc){
+			if(err) return next(err)
+			//console.log(setHash(psd),psd)
+			if(doc.length==0){
+				var myuser=new User({
+					name:username,
+					password:psd
+				})
+				myuser.save(function(err){
+					req.session.user=username
+					res.redirect('/home')
+				})
+			}else{
+				res.redirect('/register')
+			}
+			
+		})
+	}else{
+		res.redirect('/register')
+	}
+})
+
+router.get('/home',function(req, res, next){
+
+	res.render('home');
+})
+
+router.get('/logout',function(req, res, next){
+	req.session.user=null
+	res.redirect('/login')
+})
+
+function setHash(psd){
+	var salt=bcrypt.genSaltSync(6)
+	var hash=bcrypt.hashSync(psd,salt)
+	return hash 
+}
 
 module.exports = router;
